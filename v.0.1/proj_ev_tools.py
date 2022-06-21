4ae69557e0dd2d06b65678e6e561cc63a9a51b75
Tomás Pérez <54460449+licTomasPerez@users.noreply.github.com>
2022-06-21 13:50:04 -0300

    Update proj_ev_tools.py

--- v.0.1/proj_ev_tools.py
+++ v.0.1/proj_ev_tools.py
@@ -8,9 +8,54 @@ import numpy as np
 import scipy.optimize as opt 
 import pickle
 
-
 # In[2]:
 
+id2 = qutip.qeye(2)
+sx = .5*qutip.sigmax()
+sy = .5*qutip.sigmay()
+sz = .5*qutip.sigmaz()
+
+def one_body_spin_ops(N):
+    loc_sx_list = []
+    loc_sy_list = []
+    loc_sz_list = []
+    for n in range(N):
+        operator_list = []
+        for m in range(N):
+            operator_list.append(id2)
+        operator_list[n] = sx
+        loc_sx_list.append(qutip.tensor(operator_list))
+        operator_list[n] = sy
+        loc_sy_list.append(qutip.tensor(operator_list))
+        operator_list[n] = sz
+        loc_sz_list.append(qutip.tensor(operator_list))
+    return loc_sx_list, loc_sy_list, loc_sz_list
+
+# In [4]:
+
+def Heisenberg_hamiltonian (N, Jx=None, Jy=None, Jz=None, h=None, sx_list=None, sy_list=None, sz_list=None):
+    H = 0;
+    for n in range(N):
+        H += -0.5*h[n]*sz_list[n]
+        
+    for n in range(N-1):
+        H += -0.5 * Jx[n] * sx_list[n] * sx_list[n+1]
+        H += -0.5 * Jy[n] * sy_list[n] * sy_list[n+1]
+        H += -0.5 * Jz[n] * sz_list[n] * sz_list[n+1]
+    return H
+
+
+# In[5]:
+
+def free_particle_ops(N, H_H = None, sz_list=list):
+    loc_x_op = sum((.5 - sz_list[a])*(a+1) for a in range(N))
+    loc_p_op = 1j * (loc_x_op*H_H - H_H*loc_x_op)
+    loc_comm_xp = .5*(loc_x_op*loc_p_op + loc_p_op*loc_x_op)
+    loc_corr_xp = -1j*(loc_x_op*loc_p_op - loc_p_op*loc_x_op)
+    loc_p_dot = 1j*(H_H * loc_p_op - loc_p_op * H_H)
+    return loc_x_op, loc_p_op, loc_comm_xp, loc_corr_xp, loc_p_dot
+
+# In[6]:
 
 def prod_basis(b1, b2):
     return [qutip.tensor(b,s) for b in b1 for s in b2]
@@ -43,6 +88,97 @@ def base_orth(ops, rho0):
 
 def logM(rho):
     eigvals, eigvecs = rho.eigenstates()
+    return sum([log(vl)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs) if vl > 0])
+
+def sqrtM(rho):
+    eigvals, eigvecs = rho.eigenstates()
+    return sum([(abs(vl)**.5)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs)])
+
+def proj_op(K, basis, rho0):
+    return sum([scalar_prod(b, K,rho0) * b for b in basis])
+
+def rel_entropy(rho, sigma):
+    val = (rho*(logM(rho)-logM(sigma))).tr()
+    if abs(val.imag)>1.e-6:
+        print("rho or sigma not positive")
+        #print(rho.eigenstates())
+        #print(sigma.eigenstates())
+    return val.real
+
+
+# In[3]:
+
+
+def bures(rho, sigma):
+    val = abs((sqrtM(rho)*sqrtM(sigma)).tr())
+    val = max(min(val,1.),-1.)
+    return np.arccos(val)/pi
+        
+def maxent_rho(rho, basis):   
+    def test(x, rho, basis):
+        k = sum([-u*b for u,b in zip(x, basis)])        
+        sigma = (.5*(k+k.dag())).expm()
+        sigma = sigma/sigma.tr()
+        return rel_entropy(rho, sigma)    
+    res = opt.minimize(test,zeros(len(basis)),args=(rho,basis))
+    k = sum([-u*b for u,b in zip(res.x, basis)])        
+    sigma = (.5*(k+k.dag())).expm()
+    sigma = sigma/sigma.tr()
+    return sigma
+ 
+def error_maxent_state(rho, basis, distance=bures):
+    try:
+        sigma = maxent_rho(rho, basis)
+        return distance(rho,sigma)
+    except:
+        print("fail error max-ent state")
+        return None
+       
+def error_proj_state(rho, rho0, basis, distance=bures):
+    try:
+        basis = base_orth(basis, rho0)
+    except:
+        print("orth error")
+        raise
+    try:
+        sigma = proj_op(logM(rho), basis, rho0).expm()
+        sigma = (sigma+sigma.dag())/(2.*sigma.tr())
+    except:
+        print("gram error")
+    try:
+        return distance(rho, sigma)
+    except:
+        print("fail error proj state")
+        return None
+
+    # In [6]:
+
+def spin_dephasing(gamma):
+    loc_c_op_list = []
+    collapse_weights = gamma * np.ones(N)
+    for n in range(N):
+        if collapse_weights[n] > 0.0:
+           loc_c_op_list.append(np.sqrt(collapse_weights[n]) * sz_list[n])
+    return loc_c_op_list
+
+# In [7]: 
+
+coeff_matrix = ((1,1,1),
+                 (1/3,1/3,1/3),
+                 (1/3,1/3,1/3),
+                 (1/3,1/3,1/3),)
+
+def initial_state_gng(x = .5, psi0 = None, gaussian = True):
+    K1 = 0; K2 = 0; K3 = 0; loc_rho0=0
+    if gaussian: 
+        K1 = sum(coeff_matrix[1][a]*sx_list[a] for a in range(N))
+        K2 = sum(coeff_matrix[2][a]*sy_list[a] for a in range(N))
+        K3 = sum(coeff_matrix[3][a]*sz_list[a] for a in range(N))
+        loc_rho0 = (globalid+K1+K2+K3).expm()
+    else:
+        loc_rho0 = psi0 * psi0.dag()
+        loc_rho0 = x * rho0/rho0.tr() + (1-x) * globalid * .5**N
+    return loc_rho0
     return sum([log(vl)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs) if vl > 0])
 
 def sqrtM(rho):
