28f040a5a475eb8f31b23a0085711623ac074fe7
Tomás Pérez <54460449+licTomasPerez@users.noreply.github.com>
2022-06-20 22:40:39 -0300

    Updating proj_ev_tools

--- v.0.1/proj_ev_tools.py
+++ v.0.1/proj_ev_tools.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 import qutip
-from numpy import *
+import numpy 
 import scipy.optimize as opt 
 import pickle
 
@@ -13,44 +13,44 @@ import pickle
 
 
 def prod_basis(b1, b2):
-  return [qutip.tensor(b,s) for b in b1 for s in b2]
+    return [qutip.tensor(b,s) for b in b1 for s in b2]
 
 def scalar_prod(op1, op2, rho0 = None):
-  if op1.dims[0][0]!=op2.dims[0][0]:
-    return "Incompatible Qobj dimensions"
-  if rho0 is None:
-    rho0 = qutip.qeye(op1.dims[0])/op1.dims[0][0]
-  result = .5*(rho0*(op1*op2.dag()+op2.dag()*op1)).tr()
-  result = result.real
-  return result
+    if op1.dims[0][0]!=op2.dims[0][0]:
+        return "Incompatible Qobj dimensions"
+    if rho0 is None:
+        rho0 = qutip.qeye(op1.dims[0])/op1.dims[0][0]
+    result = .5*(rho0*(op1*op2.dag()+op2.dag()*op1)).tr()
+    result = result.real
+    return result
 
 def base_orth(ops, rho0):
-  if isinstance(ops[0], list):
-    ops = [op for op1l in ops for op in op1l]
-  #print(type(ops),type(ops[0]))
-  dim = ops[0].dims[0][0]
-  #print("dim=",dim)
-  basis = []
-  for i, op in enumerate(ops): 
-    alpha = [scalar_prod(op2, op, rho0) for op2 in basis]
-    op_mod = op - sum([c*op2 for c, op2, in zip(alpha, basis)])
-    op_norm = sqrt(scalar_prod(op_mod,op_mod,rho0))
-    if op_norm<1.e-12:
-      continue
-    op_mod = op_mod/(op_norm)
-    basis.append(op_mod)
-  return basis
+    if isinstance(ops[0], list):
+        ops = [op for op1l in ops for op in op1l]
+      #print(type(ops),type(ops[0]))
+    dim = ops[0].dims[0][0]
+      #print("dim=",dim)
+    basis = []
+    for i, op in enumerate(ops): 
+        alpha = [scalar_prod(op2, op, rho0) for op2 in basis]
+        op_mod = op - sum([c*op2 for c, op2, in zip(alpha, basis)])
+        op_norm = sqrt(scalar_prod(op_mod,op_mod,rho0))
+        if op_norm<1.e-12:
+            continue
+        op_mod = op_mod/(op_norm)
+        basis.append(op_mod)
+    return basis
 
 def logM(rho):
-  eigvals, eigvecs = rho.eigenstates()
-  return sum([log(vl)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs) if vl > 0])
+    eigvals, eigvecs = rho.eigenstates()
+    return sum([log(vl)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs) if vl > 0])
 
 def sqrtM(rho):
-  eigvals, eigvecs = rho.eigenstates()
-  return sum([(abs(vl)**.5)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs)])
+    eigvals, eigvecs = rho.eigenstates()
+    return sum([(abs(vl)**.5)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs)])
 
 def proj_op(K, basis, rho0):
-  return sum([scalar_prod(b, K,rho0) * b for b in basis])
+    return sum([scalar_prod(b, K,rho0) * b for b in basis])
 
 def rel_entropy(rho, sigma):
     val = (rho*(logM(rho)-logM(sigma))).tr()
@@ -67,7 +67,7 @@ def rel_entropy(rho, sigma):
 def bures(rho, sigma):
     val = abs((sqrtM(rho)*sqrtM(sigma)).tr())
     val = max(min(val,1.),-1.)
-    return .arccos(val)/pi
+    return np.arccos(val)/pi
         
 def maxent_rho(rho, basis):   
     def test(x, rho, basis):
@@ -93,13 +93,13 @@ def error_proj_state(rho, rho0, basis, distance=bures):
     try:
         basis = base_orth(basis, rho0)
     except:
-      print ("orth error")
-      raise
+        print("orth error")
+        raise
     try:
         sigma = proj_op(logM(rho), basis, rho0).expm()
         sigma = (sigma+sigma.dag())/(2.*sigma.tr())
     except:
-      print("gram error")
+        print("gram error")
     try:
         return distance(rho, sigma)
     except:
