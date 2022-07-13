f6db747c984bbbae544ff8f9b0109cc18b30ed9b
Tomás Pérez <54460449+licTomasPerez@users.noreply.github.com>
2022-07-13 12:53:45 -0300

    Update proj_ev_tools.py

--- v.0.1/Scaling-up/proj_ev_tools.py
+++ v.0.1/Scaling-up/proj_ev_tools.py
@@ -1,4 +1,4 @@
-# In [1]:
+ # In [1]:
 
 import qutip
 import numpy as np
@@ -152,22 +152,23 @@ def n_body_basis(gr, N):
     globalid_list, sx_list, sy_list, sz_list = one_body_spin_ops(N)
     
     if (isinstance(gr,int) and str(gr) in natural):
-        if (gr == 1):
-            basis = globalid_list + sx_list + sy_list + sz_list
-        elif (gr > 1):
-            basis = [op1*op2 for op1 in n_body_basis(gr-1, N) for op2 in n_body_basis(1, N)]
-    else:
-        basis = 'beep boop, gr must be natural'
+        try:
+            if (gr == 1):
+                basis = globalid_list + sx_list + sy_list + sz_list
+            elif (gr > 1):
+                basis = [op1*op2 for op1 in n_body_basis(gr-1, N) for op2 in n_body_basis(1, N)]
+        except Exception as ex:
+            basis = None
+            print(ex)
     return basis
 
 # In [9]:
 
-def n_body_max_ent_state(gr, N, coeffs = list, visualization = False):
+def n_body_max_ent_state(gr, N, coeffs = list, build_all = True, visualization = False):
     K = 0; rho_loc = 0;
-    loc_global_id_list = one_body_spin_ops(N)[0]
-    sx_list = one_body_spin_ops(N)[1];
-    sy_list = one_body_spin_ops(N)[2];
-    sz_list = one_body_spin_ops(N)[3];
+    
+    loc_globalid = qutip.tensor([qutip.qeye(2) for k in range(N)]) 
+    loc_id_list, sx_list, sy_list, sz_list = one_body_spin_ops(N)
     pauli_vec = [sx_list, sy_list, sz_list];
     
     if (gr == 1):
@@ -176,19 +177,20 @@ def n_body_max_ent_state(gr, N, coeffs = list, visualization = False):
                                     for n in range(len(one_body_spin_ops(N)))
                                     for m in range(len(one_body_spin_ops(N)[n]))
                    ) 
+            K += 10**-6 * loc_globalid
         except Exception as exme1:
             print(exme1, "Max-Ent 1 Failure")
-            raise ex
+            raise exme1
     elif (gr == 2): 
         try:
-            K += sum(coeffs[n][m] * all_two_body_spin_ops(N)[n][m] 
-                    for n in range(len(all_two_body_spin_ops(N)))
-                    for m in range(len(all_two_body_spin_ops(N)[n]))
+            K += sum(coeffs[n][m] * two_body_spin_ops(N, build_all)[n][m] 
+                    for n in range(len(two_body_spin_ops(N, build_all)))
+                    for m in range(len(two_body_spin_ops(N, build_all)[n]))
                    )
-            K += loc_global_id_list[0]
+            K += 10**-6 * loc_globalid
         except Exception as exme2:
             print(exme2, "Max-Ent 2 Failure")
-            raise ex
+            raise exme2
     else:
         print('gr must be either 1 or 2')
     
@@ -199,9 +201,119 @@ def n_body_max_ent_state(gr, N, coeffs = list, visualization = False):
         pass
     else:  
         rho_loc = None 
-        print("The result is not a density operator")
+        raise Exception("The result is not a density operator")
         
     if visualization: 
         qutip.hinton(rho_loc)
         
     return rho_loc 
+
+#In [10]:
+
+def prod_basis(b1, b2):
+    return [qutip.tensor(b,s) for b in b1 for s in b2]
+
+def scalar_prod(op1, op2, rho0 = None, HS_prod_modified = True):
+    if op1.dims[0][0]==op2.dims[0][0]:
+        pass
+    else:
+        raise Exception("Incompatible Qobj dimensions")
+    if rho0 is None:
+        rho0 = qutip.qeye(op1.dims[0])/op1.dims[0][0]
+    if (HS_prod_modified):  
+        result = .5*(rho0*(op1*op2.dag()+op2.dag()*op1)).tr()
+    else: 
+        result = .5*(rho0*(op1.dag()*op2)).tr()
+    return result.real
+
+def base_orth(ops, rho0):
+    if isinstance(ops[0], list):
+        ops = [op for op1l in ops for op in op1l]
+    dim = ops[0].dims[0][0]
+    basis = []
+    for i, op in enumerate(ops): 
+        alpha = [scalar_prod(op2, op, rho0) for op2 in basis]
+        op_mod = op - sum([c*op2 for c, op2, in zip(alpha, basis)])
+        op_norm = np.sqrt(scalar_prod(op_mod,op_mod,rho0))
+        if op_norm<1.e-12:
+            continue
+        op_mod = op_mod/(op_norm)
+        basis.append(op_mod)
+    return basis
+
+def logM(rho):
+    if me.ev_checks(rho):
+        pass
+    else:
+        raise Exception("Singular input matrix")
+    eigvals, eigvecs = rho.eigenstates()
+    return sum([np.log(vl)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs)])
+
+def sqrtM(rho):
+    if qutip.isherm(rho) and ev_checks(rho):
+        pass
+    else:
+        raise Exception("Non-hermitian or singular input matrix")
+    eigvals, eigvecs = rho.eigenstates()
+    return sum([(vl**.5)*vc*vc.dag() for vl, vc in zip(eigvals, eigvecs)])
+
+def proj_op(K, basis, rho0):
+    return sum([scalar_prod(b, K,rho0) * b for b in basis])
+
+def rel_entropy(rho, sigma):
+    if (ev_checks(rho) and ev_checks(sigma)):
+        pass
+    else:
+        raise Exception("Either rho or sigma non positive")
+    
+    val = (rho*(logM(rho)-logM(sigma))).tr()
+                    
+    if (abs(val.imag)>1.e-6):
+        val = None
+        raise Exception("Either rho or sigma not positive")
+    return val.real
+                
+# In [11]:
+
+def bures(rho, sigma):
+    if is_density_op(rho) and is_density_op(sigma):
+        val = abs((sqrtM(rho)*sqrtM(sigma)).tr())
+        val = max(min(val,1.),-1.)
+    return np.arccos(val)/np.pi
+        
+def maxent_rho(rho, basis):   
+    def test(x, rho, basis):
+        k = sum([-u*b for u,b in zip(x, basis)])        
+        sigma = (.5*(k+k.dag())).expm()
+        sigma = sigma/sigma.tr()
+        return rel_entropy(rho, sigma)    
+    res = opt.minimize(test,zeros(len(basis)),args=(rho,basis))
+    k = sum([-u*b for u,b in zip(res.x, basis)])        
+    sigma = (.5*(k+k.dag())).expm()
+    sigma = sigma/sigma.tr()
+    return sigma
+ 
+def error_maxent_state(rho, basis, distance=bures):
+    try:
+        sigma = maxent_rho(rho, basis)
+        return distance(rho,sigma)
+    except:
+        print("fail error max-ent state")
+        return None
+       
+def error_proj_state(rho, rho0, basis, distance=bures):
+    try:
+        basis = base_orth(basis, rho0)
+    except:
+        print("orth error")
+        raise
+    try:
+        sigma = proj_op(logM(rho), basis, rho0).expm()
+        sigma = (sigma+sigma.dag())/(2.*sigma.tr())
+    except:
+        print("gram error")
+    try:
+        return distance(rho, sigma)
+    except:
+        print("fail error proj state")
+        return None
