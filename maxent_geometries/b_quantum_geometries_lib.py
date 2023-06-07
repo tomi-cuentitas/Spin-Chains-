cd5f3e3ef67a8511cd62141d8010fd9caedb3a99
Tomás Pérez <54460449+tomi-cuentitas@users.noreply.github.com>
2023-06-07 09:07:23 -0300

    Update b_quantum_geometries_lib.py

--- maxent_geometries/b_quantum_geometries_lib.py
+++ maxent_geometries/b_quantum_geometries_lib.py
@@ -127,57 +127,5 @@ def fetch_Kubo_inner_prod(sigma):
 def fetch_induced_geo_distance(innerprod):
     local_dop = op1 - op2
     return np.sqrt(innerprod(local_dop, local_dop))
-
-# In [5]:
-
-def gram_matrix(basis: list, innnerprod: callable, as_qutip_qobj = True):
-    """
-    This module computes the Gram matrix associated to a basis of observables, for a quantum system, and a specific inner product.
-    This module takes as input:
-        *♥*♥* 1. basis: a list of quantum observables, in qutip.Qobj format.
-        *♥*♥* 2. innerprod: the induced inner product, on the space of observables.
-        *♥*♥* 3. (optional) as_qutip_qobj: boolean, default value: True.
-                                            A boolean option which, if toggled on, returns the Gram matrix as a qutip.Qobj
-        
-        ====> Returns: the Gram matrix as an np.array.
-        
-        Warnings: (*). all entries must be square matrices. 
     
-    """
-    size = len(basis) 
-    gram = np.array([[innerprod(op1, op2).round(14) for op2 in basis] for op1 in basis])
-    if as_qutip_qobj:
-        gram = qutip.Qobj(gram)
-    return gram
     
-def orthogonalize_basis(basis: list, innerprod: callable, idop: Qobj = None, tol = 1e-5):
-    """
-    This module orthonormalizes
-        *♥*♥* 1. sigma: a qutip.Qobj, namely a quantum state. 
-        
-        ====> Returns: a lambda function, given in terms of the state's eigenvalues and eigenvectors. 
-    
-    """
-    local_basis = basis
-    if idop:
-        sqidnorm = innerprod(idop, idop)
-        id_comp = [innerprod(idop, op)/sqidnorm for op in basis]
-        local_basis = [idop * sqidnorm ** (-.5)] + [op - mu for mu, op in zip(id_comp, basis)]
-    
-    gram = gram_matrix(local_basis, innerprod, False)
-    evals, evecs = np.linalg.eigh(gram)
-    evecs = [vec/np.linalg.norm(vec) for vec in evecs.transpose()]
-    return [
-            mu ** (-.5) * sum( c* op for c,op in zip(v, basis))
-                for mu, v in zip(evals, evecs) if p >= tol
-        ]
-
-def project_op(op: Qobj, orth_basis: list, innerprod: callable):
-    """
-    This module computes
-        *♥*♥* 1. sigma: a qutip.Qobj, namely a quantum state. 
-        
-        ====> Returns: a lambda function, given in terms of the state's eigenvalues and eigenvectors. 
-    
-    """
-    return np.array([innerprod(qop, op) for qop in orth_basis])
